0890cc84042f2bfc35b394d80236ab6cfa9b6dac
Mark-Sarmiento <markrafaelsarmiento9@gmail.com>
2023-06-18 20:21:37 +0800

    init

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename='https://mark-sarmiento.github.io/Nutriculture/'>
+  <BrowserRouter >
     <App />
   </BrowserRouter>
 );
